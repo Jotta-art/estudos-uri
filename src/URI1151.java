91c0c18e4fa26372950582e95518509829014d53
João Pedro <joao.pedro42@outlook.com.br>
2021-04-23 12:37:17 -0300

    Tentando resolver o URI1151

--- src/URI1151.java
+++ src/URI1151.java
@@ -1,13 +1,33 @@
 import java.util.Scanner;
 
 public class URI1151 {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int num = sc.nextInt();
+        int seq = 0;
+        int penultimo = 0;
+        int ultimo = 0;
+        int cont1 = 1;
+        int cont2 = 2;
 
         for (int i = 0; i < num; i++) {
-
+            if(i > 0){
+                if (i > 2 && i % 2 != 0) {
+                    cont1++;
+                }
+                ultimo = i - cont1;
+            }
+            if (i > 1) {
+                if (i > 3 && i % 2 == 0) {
+                    cont2++;
+                }
+                penultimo = i - cont2;
+                seq = penultimo + ultimo;
+            }
+            System.out.print(seq + "\t");
+            seq++;
         }
     }
 }
