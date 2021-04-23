7a8a42cf8e42b02e443f73c98c13f83b52d0d04f
João Pedro <joao.pedro42@outlook.com.br>
2021-04-23 11:32:36 -0300

    Subindo arquivos

---
+++ src/URI1151.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class URI1151 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+
+        for (int i = 0; i < num; i++) {
+
+        }
+    }
+}
